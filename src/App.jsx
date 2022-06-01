234d54f08bc21a418ba94784681406cce76b2d3d
joaquinn124 <joaquinncastro@gmail.com>
2022-05-31 23:46:18 -0300

    desafio 2

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Body from './components/Body/Body';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+    
+  );
+}
+
+export default App;
