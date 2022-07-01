49933936ac8e95cddb5aca32f6e700d9949e0bcc
joaquinn124 <joaquinncastro@gmail.com>
2022-07-01 00:03:26 -0300

    Primera Entrega Proyecto Final

--- src/helpers/productsList.js
+++ src/helpers/productsList.js
@@ -2,17 +2,17 @@
 let productsList = [
     {
         id: '1',
-        model: 'Adidas Predator Freak.1',
-        series: 'Predator',
-        collection: 'limited',
-        price: 180,
-        stock: 5500,
-        imageUrl: '/media/products/Adidas_Predator_Freak.1 SG.jpg',
-        description: 'Every time you cross that white line, you enter a whole new world. An alternative reality where you call the shots. The pitch is yours to control. Set your inner freak free in adidas Predator.'
+        model: 'Nike Tiempo Legend 9 Elite',
+        series: 'Tiempo',
+        collection: 'regular',
+        price: 231,
+        stock: 5,
+        imageUrl: '/media/products/Nike_Tiempo_Legend_9 Elite.jpg',
+        description: 'One of our lightest Tiempos yet, the Nike Tiempo Legend 9 Elite FG lets you go forward in a low-top design that`s reimagined for attack.'
     },
     {
         id: '2',
-        model: 'Nike Mercurial Vapor 14',
+        model: 'Nike Mercurial Vapor 14 FG',
         series: 'Mercurial Vapor',
         collection: 'regular',
         price: 130,
@@ -22,7 +22,7 @@ let productsList = [
     },
     {
         id: '3',
-        model: 'Puma Ultra 1.3 MXSG Pro',
+        model: 'Puma Ultra 1.3 MXSG Pro FG',
         series: 'Ultra',
         collection: 'regular',
         price: 160,
@@ -32,7 +32,7 @@ let productsList = [
     },
     {
         id: '4',
-        model: 'New Balance Tekela V3+',
+        model: 'New Balance Tekela V3+ Aura',
         series: 'Tri Aura Pack',
         collection: 'limited',
         price: 190,
@@ -42,23 +42,83 @@ let productsList = [
     },
     {
         id: '5',
-        model: 'Joma Top Flex 2202 In',
-        series: 'Top Flex',
+        model: 'Adidas X Speedflow.1 FG AG',
+        series: 'Speedflow',
         collection: 'regular',
-        price: 55,
-        stock: 100,
-        imageUrl: '/media/products/Joma_Top_Flex_2202_In.jpg',
-        description: 'The upper part of these Joma futsal shoes is made of high quality and very flexible leather. This boots incorporate PROTECTION technology in the toecap to reinforce their resistance and stability when hitting.'
+        price: 130,
+        stock: 10,
+        imageUrl: '/media/products/Adidas_X_Speedflow.1 FG.jpg',
+        description: 'Speedflow with laceless closure system, which provides a fine touch surface and a clean hit on the ball. Lightweight construction with Carbitex carbon fibers in the forefoot, which creates an efficient power discharge at high speed.'
     },
     {
         id: '6',
-        model: 'Diadora Pichichi 3 MG 14',
-        series: 'Pichichi',
+        model: 'Puma Future 1.2 FG Magenta',
+        series: 'Future',
         collection: 'regular',
-        price: 35,
+        price: 160,
         stock: 20,
-        imageUrl: '/media/products/Diadora_Pichichi_3_MG 14.jpg',
-        description: 'The perfect pair of boots for training and football matches with your friends. With water-resistant Suprell upper, anti-stretch lining and insole with flex points on the ball of the foot guarantee flexibility and control.'
+        imageUrl: '/media/products/Puma_Future_1.2 FGAG Magenta.jpg',
+        description: 'Unbeatable features, including PUMA FUZIONFIT compression band, ultra-lightweight materials, a textured upper for ball grip and the Dynamic Motion System outsole combine to keep your game as dynamic as can be.'
+    },
+    {
+        id: '7',
+        model: 'Nike The Nike Premier 1971',
+        series: 'Premier',
+        collection: 'limited',
+        price: 170,
+        stock: 50,
+        imageUrl: '/media/products/Nike_The_Nike_Premier_1971.jpg',
+        description: 'In 1971, the iconic Swoosh first appeared on the side of a boot, and 50 years later, Nike pays homage to the legacy of football with a remake. Completely unique and limited football boot, with an upper consisting of premium leather 1971 pairs worldwide.'
+    },
+    {
+        id: '8',
+        model: 'Adidas Predator Edge Crystal FG',
+        series: 'Predator',
+        collection: 'limited',
+        price: 349,
+        stock: 5,
+        imageUrl: '/media/products/Adidas_Predator_Edge_Crystal FG.jpg',
+        description: 'Design decorated with Swarovski crystal details, which creates an eye-catching and icy look on the court. Primeknit upper with innovative and revolutionary Zone Skin, which provides ultimate control and improved grip Forefoot with Powerfacet technology incorporated.'
+    },
+    {
+        id: '9',
+        model: 'Adidas Predator Edge Geometric FG',
+        series: 'Predator',
+        collection: 'limited',
+        price: 255,
+        stock: 15,
+        imageUrl: '/media/products/Adidas_Predator_Edge_Geometric FG.jpg',
+        description: 'Used by French midfield creator Paul Pogba and female star Toni Duggan. Primeknit upper with innovative and revolutionary Zone Skin, which provides ultimate control and improved grip Forefoot with Powerfacet technology incorporated.'
+    },
+    {
+        id: '10',
+        model: 'Lotto Solista 100 IV Gravity',
+        series: 'Future',
+        collection: 'limited',
+        price: 160,
+        stock: 20,
+        imageUrl: '/media/products/Lotto_Solista_100_IV_Gravity.jpg',
+        description: 'Laceless Lotto Solista with a synthetic upper for a comfortable fit. Clean and agile design for the paced player with PuntoFlex@ tech for full flexibility. Elastic sock for full lockdown.'
+    },
+    {
+        id: '11',
+        model: 'Nike Phantom GT Elite 3D FG',
+        series: 'Phantom',
+        collection: 'limited',
+        price: 249,
+        stock: 7,
+        imageUrl: '/media/products/Nike_Phantom_GT_Elite 3D FG.jpg',
+        description: 'Nike Phantom GT is the latest innovation that helps you unlock your full potential, giving you everything you need to finish off the games. Upper made from Generative Texture, that delivers extreme grip and control.'
+    },
+    {
+        id: '12',
+        model: 'Pantofola D´oro Del Duca White FG',
+        series: 'Del Duca',
+        collection: 'limited',
+        price: 290,
+        stock: 5,
+        imageUrl: '/media/products/Pantofola_Dioro_Del_Duca_White FG.jpg',
+        description: 'Exclusive Limited Edition of The Croc in the form of Superleggera with crocodile markings in the design. The upper consists of calf leather, which reduces water absorption and provides unsurpassed comfort.'
     },
 ];
 
